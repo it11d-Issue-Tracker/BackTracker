011bf915a6e3c6c4e54caef609373b63e62c6715
matrix1414 <marti.tarres.pinedo@estudiantat.upc.edu>
2025-04-10 14:34:23 +0200

    primera implementacio de settings

--- Proyecto/Taiga2/Issue_Tracker/models.py
+++ Proyecto/Taiga2/Issue_Tracker/models.py
@@ -4,36 +4,44 @@ from django.contrib.auth.models import User
 import uuid
 
 
-class Issue(models.Model):
-    STATUS_CHOICES = [
-        ('new', 'New'),
-        ('in_progress', 'In Progress'),
-        ('ready_for_test', 'Ready for Test'),
-        ('closed', 'Closed'),
-        ('archived', 'Archived'),
-    ]
+class Status(models.Model):
+    id = models.CharField(max_length=100, primary_key=True)
 
-    PRIORITY_CHOICES = [
-        ('low', 'Low'),
-        ('normal', 'Normal'),
-        ('high', 'High'),
-        ('critical', 'Critical'),
-    ]
-
-    id_issue = models.AutoField(primary_key=True, editable=False)
-    title = models.CharField(max_length=200)
-    description = models.TextField()
-    status = models.CharField(max_length=100, choices=STATUS_CHOICES, default='new')
-    priority_id = models.CharField(max_length=100, choices=PRIORITY_CHOICES, default='normal')
-    created_by = models.ForeignKey(User, on_delete=models.CASCADE, db_column='created_by', to_field='id', related_name='issues')
-    assigned_to = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='assigned_issues', db_column='assigned_to')
-    deadline = models.DateField(null=True, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
+    def __str__(self):
+        return self.id
 
     class Meta:
-        db_table = 'issue'
+        db_table = 'status'
+
+
+class Priority(models.Model):
+    id = models.CharField(max_length=100, primary_key=True)
+
+    def __str__(self):
+        return self.id
+
+    class Meta:
+        db_table = 'priority'
+
+
+
+
+class Issue(models.Model):
+        id_issue = models.AutoField(primary_key=True, editable=False)
+        title = models.CharField(max_length=200)
+        description = models.TextField()
+        status = models.ForeignKey('Status', on_delete=models.SET_NULL, null=True, related_name='issues')
+        priority = models.ForeignKey('Priority', on_delete=models.SET_NULL, null=True, related_name='issues')
+        created_by = models.ForeignKey(User, on_delete=models.CASCADE, db_column='created_by', to_field='id',
+                                       related_name='issues')
+        assigned_to = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='assigned_issues',
+                                        db_column='assigned_to')
+        deadline = models.DateField(null=True, blank=True)
+        created_at = models.DateTimeField(auto_now_add=True)
+        updated_at = models.DateTimeField(auto_now=True)
+
+        class Meta:
+            db_table = 'issue'
 
 
 class Comment(models.Model):
